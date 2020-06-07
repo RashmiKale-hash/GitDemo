d3e34705b60e7fc900e2987c4ca3ab4e9bc52d8d
Rashmi <rashmikale11@gmail.com>
2020-06-07 21:28:52 +0530

    First commit

---
+++ src/test/java/qaclickacademy/seleniumtest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+
+public class seleniumtest {
+	
+	@org.testng.annotations.Test
+	public void Browserautomation()
+	{
+		System.out.println("Browserautomation");
+	}
+	
+
+	@org.testng.annotations.Test
+	public void DummyUI()
+	{
+		System.out.println("dummyUI");
+	}
+}
